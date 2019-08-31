7cdaf4f6d541b0b874f42c935e79e96d89e449ca
Luther <luther0211@hotmail.com>
2019-08-31 17:18:43 -0500

    basic client/server communication setup

--- server.js
+++ server.js
@@ -17,9 +17,13 @@ app.get('/api', (req, res) => {
     res.json({data: 'OK'})
 })
 
-app.use(() => {
-    express.static(path.join(__dirname, 'client', 'build'))
-    console.log('app use endpoint working')
+app.use('/', (req, res) => {
+    res.send('WORKING')
 })
+
+// app.use(() => {
+//     express.static(path.join(__dirname, 'client', 'build'))
+//     console.log('app use endpoint working')
+// })
 
 app.listen(PORT, () => console.log(`: : : : : : : : SERVER LISTENING ON PORT ${PORT} : : : : : : : :`))
