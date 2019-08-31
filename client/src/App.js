8cfa234088a899696ad0d97c046bf130a5a419ae
Luther <luther0211@hotmail.com>
2019-08-31 17:11:36 -0500

    basic client/server communication setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+    const [state, setState] = useState({
+        data: false
+    })
+
+    useEffect(() => {
+        if(!state.data) {
+            apiTest()
+        }
+    })
+
+    const apiTest = () => {
+        fetch('http://localhost:8080/api')
+            .then(res => res.json())
+            .then(data => setState({...data}))        
+    }
+    
   return (
     <div className="App">
-        APP
+        fetch data from server: { state.data }
     </div>
   );
 }
--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const path = require('path')
 
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 const app = express();
 
+//CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+  });
 
 app.get('/api', (req, res) => {
-    res.json({TEST: 'OK'})
+    res.json({data: 'OK'})
 })
 
 app.use(() => {
