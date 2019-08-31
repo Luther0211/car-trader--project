7d35b74c69c6bb4ef91deae05042b63ca7a298d6
Luther <luther0211@hotmail.com>
2019-08-31 16:38:34 -0500

    Testing endpoints

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ require('dotenv').config()
 const PORT = process.env.PORT || 3000
 const app = express();
 
+
+app.get('/api', (req, res) => {
+    res.json({TEST: 'OK'})
+})
+
 app.use(() => {
     express.static(path.join(__dirname, 'client', 'build'))
     console.log('app use endpoint working')
 })
-
-
-
 
 app.listen(PORT, () => console.log(`: : : : : : : : SERVER LISTENING ON PORT ${PORT} : : : : : : : :`))
