f31fa45fe41fea7aeb996f6fae5c0fba4dec72c8
Luther <luther0211@hotmail.com>
2019-09-02 20:07:32 -0500

    Created badge to user icon in Navbar component

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -9,6 +9,7 @@ const Navbar = () => {
                 <a href="#" class="Navbar__main-link navbar-brand p-0">Car Trader</a>
                 <a href="#" class="Navbar__user-link position-relative">
                     <i class="fas fa-user-circle"></i>
+                    <span class="Navbar__user-number badge badge-pill position-absolute">12</span>
                 </a>
             </div>
         </nav>
