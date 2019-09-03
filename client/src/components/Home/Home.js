eadac719e6beddeadba152133720724fe46cf137
Luther <luther0211@hotmail.com>
2019-09-03 18:27:15 -0500

    Updated Home form: made it responsive / added margins

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <div className="Home">
             <div className="Home__main-section jumbotron rounded-0">
 
-                <div className="container">
+                <div className="Home__main-container container">
                     <h1 className="d-none d-md-block display-4 m-0 text-light">Search cars for sale</h1>
                     <h1 className="d-block d-md-none text-light m-0">Search cars for sale</h1>
                     
@@ -17,22 +17,22 @@ const Home = () => {
                     
                     <form className="mb-5">
                         <div className="form-row">
-                            <div className="col-4">
+                            <div className="col-12 col-sm-6 col-md-4 mb-2">
                                 <select className="form-control" defaultValue="any">
                                     <option value="any">Any Make</option>
                                 </select>
                             </div>
-                            <div className="col-3">
+                            <div className="col-12 col-sm-6 col-md-3 mb-2">
                                 <select className="form-control">
                                     <option value="any">Any Condition</option>
                                     <option value="new">New</option>
                                     <option value="used">Used</option>
                                 </select>
                             </div>
-                            <div className="col-3">
+                            <div className="col-12 col-sm-6 col-md-3 mb-2">
                                 <input className="form-control" type="text" placeholder="ZIP Code (0-9)" name="zip" pattern="[0-9]*" />
                             </div>
-                            <div className="col-2">
+                            <div className="col-12 col-sm-6 col-md-2 mb-2">
                                 <button className="btn btn-info w-100" type="submit">Search</button>
                             </div>
                         </div>
