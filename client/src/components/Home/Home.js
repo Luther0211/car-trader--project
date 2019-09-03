b68484d3af0dbf872b2e37caa77994c0386ebf48
Luther <luther0211@hotmail.com>
2019-09-03 17:21:14 -0500

    Added background image to Home Component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -7,12 +7,13 @@ const Home = () => {
 
     return (
         <div className="Home">
-            <div className="Home__main-section jumbotron">
+            <div className="Home__main-section jumbotron rounded-0">
 
                 <div className="container">
-                    <h1 className="display-4">Search cars for sale</h1>
+                    <h1 className="d-none d-md-block display-4 text-light">Search cars for sale</h1>
+                    <h1 className="d-block d-md-none">Search cars for sale</h1>
                     
-                    <hr className="my-4" />
+                    <hr className="my-4 border-light" />
                     
                     
                 </div>
