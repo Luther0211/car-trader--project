6b1a4e5d204de89c017ac4a8ca6dcfbd497b15a0
Luther <luther0211@hotmail.com>
2019-09-05 17:36:29 -0500

    Updated Results Component: Created result information bar

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -5,7 +5,32 @@ const Results = () => {
 
     return (
         <div className="Results">
-            Results component
+            <div className="Results__bar">
+                <div className="container py-2">
+                    <div className="row">
+                    
+                        <div className="col-12 col-sm-6 text-center text-sm-left">
+                            <p className="Results__information mt-2">
+                                <span className="Results__number">123567</span> results in <span className="Results__zip"> <i className="fas fa-map-marker-alt"></i> 91911</span>
+                            </p>
+                        </div>
+
+                        <div className="col-12 col-sm-6 d-flex justify-content-center justify-content-sm-end">
+                            <select className="form-control">
+                                <option value="relevance">Sort by: Relevance</option>
+                                <option value="price-lowest">Sort by: Price - Lowest</option>
+                                <option value="price-highest">Sort by: Price - Highest</option>
+                                <option value="mile-lowest">Sort by: Mileage - Lowest</option>
+                                <option value="mile-highest">Sort by: Mileage - Highest</option>
+                                <option value="year-lowest">Sort by: Year - Lowest</option>
+                                <option value="year-highest">Sort by: Year - Highest</option>
+                            </select>
+                        </div>
+                    
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
