f4de916a227b2b78ca2fd6b98aa066f58805997b
Luther <luther0211@hotmail.com>
2019-09-06 16:50:14 -0500

    Added the rest of the fields inside the accordion form in Results component

--- client/src/components/Results/Results.js
+++ client/src/components/Results/Results.js
@@ -37,33 +37,156 @@ const Results = () => {
 
                 <div className="d-md-none">
                     <button className="Results__filter-btn btn btn-info">
-                        <i className="fas fa-filter"></i> Filter
+                        <i className="fas fa-sliders-h"></i> Filter
                     </button>
                 </div>
             
                 <div className="col-md-4 pl-0 d-none d-md-inline-block">
                     <div className="Results__form border border-info">
-                        <h5 className="text-center bg-info text-light py-2">Filter</h5>
+                        <h5 className="text-center bg-info text-light py-2 m-0">Filter</h5>
 
                         <form>
+                            
                             <Accordion defaultActiveKey="0">
+                                
                                 <Card>
                                     <Accordion.Toggle as={Card.Header} eventKey="0">
-                                        Click me!
+                                        <strong>Location</strong>
                                     </Accordion.Toggle>
                                     <Accordion.Collapse eventKey="0">
-                                        <Card.Body>Hello! I'm the body</Card.Body>
+                                        <Card.Body>
+                                            
+                                        </Card.Body>
                                     </Accordion.Collapse>
                                 </Card>
+
                                 <Card>
                                     <Accordion.Toggle as={Card.Header} eventKey="1">
-                                        Click me!
+                                        <strong>Price</strong>
                                     </Accordion.Toggle>
                                     <Accordion.Collapse eventKey="1">
-                                        <Card.Body>Hello! I'm another body</Card.Body>
+                                        <Card.Body>
+
+                                        </Card.Body>
                                     </Accordion.Collapse>
                                 </Card>
+
+                                <Card>
+                                    <Accordion.Toggle as={Card.Header} eventKey="2">
+                                        <strong>Condition</strong>
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="2">
+                                        <Card.Body>
+
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+
+                                <Card>
+                                    <Accordion.Toggle as={Card.Header} eventKey="3">
+                                        <strong>Year</strong>
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="3">
+                                        <Card.Body>
+
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+
+                                <Card>
+                                    <Accordion.Toggle as={Card.Header} eventKey="4">
+                                        <strong>Mileage</strong>
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="4">
+                                        <Card.Body>
+
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                                
+                                <Card>
+                                    <Accordion.Toggle as={Card.Header} eventKey="5">
+                                        <strong>Make</strong>
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="5">
+                                        <Card.Body>
+
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                                
+                                <Card>
+                                    <Accordion.Toggle as={Card.Header} eventKey="6">
+                                        <strong>Body Style</strong>
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="6">
+                                        <Card.Body>
+
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                                
+                                <Card>
+                                    <Accordion.Toggle as={Card.Header} eventKey="7">
+                                        <strong>Exterior Color</strong>
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="7">
+                                        <Card.Body>
+
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                                
+                                <Card>
+                                    <Accordion.Toggle as={Card.Header} eventKey="8">
+                                        <strong>Interior Color</strong>
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="8">
+                                        <Card.Body>
+
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                                
+                                <Card>
+                                    <Accordion.Toggle as={Card.Header} eventKey="9">
+                                        <strong>Transmission</strong>
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="9">
+                                        <Card.Body>
+
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+
+                                <Card>
+                                    <Accordion.Toggle as={Card.Header} eventKey="10">
+                                        <strong>Doors</strong>
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="10">
+                                        <Card.Body>
+                                            
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+
+                                <Card>
+                                    <Accordion.Toggle as={Card.Header} eventKey="11">
+                                        <strong>Seller Type</strong>
+                                    </Accordion.Toggle>
+                                    <Accordion.Collapse eventKey="11">
+                                        <Card.Body>
+
+                                        </Card.Body>
+                                    </Accordion.Collapse>
+                                </Card>
+                                
                             </Accordion>
+
+                            <div className="text-right">
+                                <button type="submit" className="Results__form-btn btn btn-info m-3">APPLY</button>
+                            </div>
+
                         </form>    
 
                     </div>
