e0244124586f0a82f31bf59276266f133426c659
Luther <luther0211@hotmail.com>
2019-09-13 18:54:13 -0500

    Navbar Component now renders the number of listings saved

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,14 +3,18 @@ import { Link } from 'react-router-dom'
 import './Navbar.scss'
 
 const Navbar = () => {
-
+    const listings_saved = JSON.parse(window.localStorage.getItem('car_listings'))
     return (
         <nav className="Navbar navbar bg-info">
             <div className="container">
                 <Link to="/" className="Navbar__main-link navbar-brand p-0">Auto Listings</Link>
                 <a href="#2" className="Navbar__user-link position-relative">
                     <i className="fas fa-user-circle"></i>
-                    <span className="Navbar__user-number badge badge-pill position-absolute">12</span>
+                    {
+                        listings_saved.length > 0 
+                            ? <span className="Navbar__user-number badge badge-pill position-absolute">{listings_saved.length}</span>
+                            :  ''
+                    }
                 </a>
             </div>
         </nav>
