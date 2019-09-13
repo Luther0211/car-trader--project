2bf73bfb115054752e19c24d423cf3a7e5429260
Luther <luther0211@hotmail.com>
2019-09-13 17:58:19 -0500

    Updated ResItem Component: Made it responsive for all screen sizes

--- client/src/components/ResItem/ResItem.js
+++ client/src/components/ResItem/ResItem.js
@@ -15,11 +15,11 @@ const ResItem = ({ data }) => {
         detail_list.push(<span key='point2' name='detailPoint'>•</span>)
     }
     if(data.build && data.build.fuel_type) {
-        detail_list.push(<span key='details-fuel'>{data.build.fuel_type}</span>)
+        detail_list.push(<span key='details-fuel'>{data.build.fuel_type.replace('Unleaded', '')}</span>)
         detail_list.push(<span key='point3' name='detailPoint'>•</span>)
     }
     if(data.build && data.build.transmission) {
-        detail_list.push(<span key='details-trans'>{data.build.transmission}</span>)
+        detail_list.push(<span key='details-trans'>{data.build.transmission.replace('Automated', '')}</span>)
     }
 
     if(detail_list[detail_list.length - 1].name === 'detailPoint') detail_list.pop()
@@ -34,7 +34,7 @@ const ResItem = ({ data }) => {
 
 
     return (
-        <div className='ResItem container-fluid mb-4 bg-white shadow-sm p-0'>
+        <div className='ResItem container-fluid position-relative mb-4 bg-white shadow-sm p-0'>
             <h6 className="text-light bg-info py-1 px-2 m-0">{data.heading}</h6>
 
             <div className='row'>
@@ -47,19 +47,20 @@ const ResItem = ({ data }) => {
                     />
                 </div>
 
-                <div className='col-8 col-lg-7 pl-0 pr-4 position-relative'>
-                    <h5 className="d-flex justify-content-between m-0 my-lg-3 pb-lg-3 border-bottom">
+                <div className='col-8 col-lg-7 pl-0 pr-4'>
+                    <h5 className="d-flex justify-content-between m-0 my-lg-3 py-1 pb-lg-3 border-bottom">
                         <span>{data.price ? `$${(data.price).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}` : 'Ask for Price'}</span>
                         <span className='ResItem__save-icon text-info'>{ <i className="far fa-heart" /> }</span>
                     </h5>
 
-                    <div className='ResItem__details p-2 mt-4 d-flex justify-content-between'>
+                    <div className='ResItem__details p-0 p-md-1 p-lg-2 mt-1 mt-lg-3 mt-xl-4 d-flex justify-content-between'>
                         { detail_list }
                     </div>
-                    
-                    <button className='ResItem__view-btn position-absolute mr-4 btn btn-info'>View</button>
-
                 </div>
+                    
+
+                <button className='ResItem__view-btn d-none d-sm-block position-absolute btn btn-sm btn-info'>View</button>
+                <a className="ResItem__view-link d-sm-none position-absolute text-info border-bottom border-info">View</a>
 
             </div>
         </div>
